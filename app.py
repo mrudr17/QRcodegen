8fb7982ca62c03a16e729d75029f190268eac162
MR UDAY <71985806+mrudr17@users.noreply.github.com>
2023-03-17 15:06:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,27 +17,25 @@ def generate_qr_code(data, image_size):
 def main():
     # Set app title
     
-    col1, col2 = st.columns(2)
-    
-    col1.st.title('QR Code Generator')
+    st.title('QR Code Generator')
     
     # Get input from user
-    col1.data = st.text_input('Enter the data to encode:')
-    col1.image_size = st.slider('Select image size (pixels):', 100, 1000, 400)
+    data = st.text_input('Enter the data to encode:')
+    image_size = st.slider('Select image size (pixels):', 100, 1000, 400)
     
-    col1.submit = st.button('Submit')
+    submit = st.button('Submit')
 
-    if col1.submit:
+    if submit:
     
         # Generate QR code
-        if col1.data:
+        if data:
             img = generate_qr_code(data, image_size)
-            col2.st.title('QR Code')
-            col2.st.image(img)
+            
+            st.image(img)
             buffered = BytesIO()
             img.save(buffered, format="PNG")
             byte_im = buffered.getvalue()
-            col2.btn = st.download_button(
+            btn = st.download_button(
               label="Download Image",
               data=byte_im,
               file_name="output.png",
